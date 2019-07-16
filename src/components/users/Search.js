0b48dae704e155918a320fce20ff7ba4b410a219
lacid <acidus.l@gmail.com>
2019-07-16 21:09:42 +0200

    before hooks & context

---
+++ src/components/users/Search.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class Search extends Component {
+  state = {
+    text: ''
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired
+    // setAlert: PropTypes.func.isRequired
+  };
+
+  // onSubmit = e => {
+  //   e.preventDefault();
+  //   if (this.state.text === '') {
+  //     this.props.setAlert('enter input...', 'light');
+  //   } else {
+  //     this.props.searchUsers(this.state.text);
+  //     this.setState({ text: '' });
+  //   }
+  // };
+  onSubmit = e => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
+
+  onChange = e => this.setState({ text: e.target.value });
+
+  render() {
+    const { clearUsers, showClear } = this.props;
+
+    return (
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users...'
+            value={this.state.text}
+            onChange={this.onChange}
+            required
+          />
+          <input type='submit' value='Search' className='btn btn-dark btn-block' />
+        </form>
+        {showClear && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Search;
